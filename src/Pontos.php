a8c7f167839117ad49be83bd5328ef2d16b69c23
wouerner <wouerner@protonmail.com>
2019-01-29 14:57:02 -0200

    removendo declare

--- src/Pontos.php
+++ src/Pontos.php
@@ -1,8 +1,6 @@
 <?php
 // 1. Construa um algoritmo que, tendo como dados de entrada dois pontos quaisquer no plano, P(x1,y1) e P(x2,y2),
 // escreva a distância entre eles. A fórmula que efetua tal cálculo é:
-declare(strict_types=1);
-
 namespace Wouerner;
 
 final class Pontos
